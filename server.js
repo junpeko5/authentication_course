e40f56e04506755643a1856aebfd92f729971192
junpeko5 <junpeko5@gmail.com>
2022-01-01 11:55:21 +0900

    Handling Errors

--- server.js
+++ server.js
@@ -36,12 +36,19 @@ app.post('/register', (req, res) => {
       password: req.body.password
       // In a production app, you'll want to encrypt the password
     }
-
     const data = JSON.stringify(user, null, 2)
-    var dbUserEmail = require('./db/user.json').email
 
-    if (dbUserEmail === req.body.email) {
-      res.sendStatus(400)
+    var dbUserEmail = require('./db/user.json').email
+    var errorsToSend = [] // array to collect errors
+
+    if (dbUserEmail === user.email) { // check to see if email already exists in db
+      errorsToSend.push('An account with this email already exists.')
+    }
+    if (user.password.length < 5) { // validate password is in correct format
+      errorsToSend.push('Password too short.')
+    }
+    if (errorsToSend.length > 0) { // check if there are any errors
+      res.status(400).json({ errors: errorsToSend }) // send errors back with status code
     } else {
       fs.writeFile('./db/user.json', data, err => {
         if (err) {
@@ -78,7 +85,7 @@ app.post('/login', (req, res) => {
       name: userInfo.name
     })
   } else {
-    res.sendStatus(400)
+    res.status(401).json({ error: 'Invalid login. Please try again.' })
   }
 })
 
